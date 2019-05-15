57c6d4afff2f0146febb699ce35cd7dcf17711ef
Claes Ö <claes@stenbergit.se>
2019-05-15 15:54:52 +0200

    Improvement.

--- PluginWfjsCookie.php
+++ PluginWfjsCookie.php
@@ -1,28 +1,8 @@
 <?php
-/**
-<p>
-Javascript class to handle cookies as object for easier management.
-</p>
-#code-javascript#
-#load:[web_dir]/plugin/[plugin]/include.js:load#
-#code#
- */
 class PluginWfjsCookie{
-  /**
-  <p>
-  Include in head section.
-  </p>
-  #code-yml#
-  type: widget
-  data:
-    plugin: 'wfjs/cookie'
-    method: include
-  #code#
-  */
   public static function widget_include(){
-    $element = wfDocument::createHtmlElement('script', null, array('src' => '/plugin/wfjs/cookie/include.js'));
+    wfPlugin::enable('include/js');
+    $element = wfDocument::createWidget('include/js', 'include', array('src' => '/plugin/wfjs/cookie/include.js'));    
     wfDocument::renderElement(array($element));
   }
-  
-  
 }
